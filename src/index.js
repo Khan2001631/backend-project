e3753cc029d9aa3372b050020acb09eef693651b
Mohammad Khan <fcburgers27@gmail.com>
2024-09-01 08:31:10 +0530

    Connected to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,68 @@
+// require('dotenv').config({path: './env'})
+import dontenv  from 'dotenv'
+import connectDB from "./db/index.js";
+
+dontenv.config({path: './env'})
+// You also have to make changes in script in package.json:
+// "dev": "nodemon -r dotenv/config --experimental-json-modules src/index.js"
+
+
+
+
+connectDB(); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Points to remeber:
+/*
+While connecting to the database, problems can occur, so it is best to wrap it in try-catch block or Promises.
+
+Database is always in other continent so it takes time. So apply async await
+
+While using IFFY, it is a professional approach to use semicolon before IFFY code.
+*/
+
+
+
+
+
+// One of the approach to coneect database to mongoose
+/*
+import express from "express";
+const app = express();
+(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log('Database connected');
+        app.on('error', (error) => {
+            // Write a suitable error:
+            console.error("Problem connecting express app", error);
+            throw error;
+        })
+        app.listen(process.env.PORT, () => {
+            console.log(`Server running on port ${process.env.PORT}`);
+        })
+        
+    }
+    catch(error) {
+        console.error(error);
+        throw error;
+    }
+})()
+*/
+
+
+
